dc482a8575c0d16ba516c93c6ddb39b1f2199bbf
Alex Billson <alex_billson@outlook.com>
2017-10-20 10:56:44 +1100

    Modified home layout

---
+++ app/src/main/java/com/example/strik/lafa/FlashSetAdapter.java
@@ -0,0 +1,8 @@
+package com.example.strik.lafa;
+
+/**
+ * Created by strik on 19/10/2017.
+ */
+
+public class FlashSetAdapter {
+}
--- app/src/main/java/com/example/strik/lafa/MainActivity.java
+++ app/src/main/java/com/example/strik/lafa/MainActivity.java
@@ -11,22 +11,6 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FlashCard[] cards = {
-            new FlashCard(0, "Are traps gay?", "Yes."),
-            new FlashCard(1, "Are you sure?", "Yes.")
-        };
-
-        FlashSetManager manager = new FlashSetManager(this, R.id.layout_output,
-                new FlashSet(Arrays.asList(cards), "Test Collection"));
-        manager.pushCardsToList();
-        testGson();
-    }
-
-    private void testGson()
-    {
-        String testJson = "{'id': 1, 'question': 'Test question', 'answer': 'test asnwer'}";
-        FlashCard testCard = LAFA.cardFromJson(LAFA.cardToJson(LAFA.cardFromJson(testJson)));
     }
 
     @Override
