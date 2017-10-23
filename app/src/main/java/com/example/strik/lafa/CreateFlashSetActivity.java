154edaa51d08f452ed01dcbcfaa2e85c6118d692
Alex Billson <alex_billson@outlook.com>
2017-10-23 19:14:40 +1100

    Working on FlashSet modification system

--- app/src/main/java/com/example/strik/lafa/CreateFlashSetActivity.java
+++ app/src/main/java/com/example/strik/lafa/CreateFlashSetActivity.java
@@ -1,8 +1,15 @@
 package com.example.strik.lafa;
 
+import android.content.Intent;
 import android.content.res.Resources;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CreateFlashSetActivity extends AppCompatActivity {
 
@@ -11,5 +18,48 @@ public class CreateFlashSetActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_flash_set);
         setTitle(getResources().getString(R.string.title_activity_create_flash_set));
+        findViewById(R.id.fab_create_flash_next)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (isFormValid()) {
+                            Intent submitFormIntent = new Intent(view.getContext(), MainActivity.class);
+                            submitFormIntent.putExtra("data", getFormData());
+                            startActivity(submitFormIntent);
+                        }
+                    }
+                });
+    }
+
+    /**
+     * Ensure the form is valid, apply any error checking if necessary
+     *
+     * @return true if form is valid, false otherwise
+     */
+    private boolean isFormValid() {
+        boolean isValid = true;
+
+        EditText editTextSetTitle = (EditText) findViewById(R.id.editText_set_title);
+
+        if (editTextSetTitle.getText().toString().isEmpty()) {
+            isValid = false;
+            editTextSetTitle.setError("FlashSet Title is required!");
+        }
+
+        return isValid;
+    }
+
+    /**
+     * Get the data of the form as a HashMap
+     *
+     * @return
+     */
+    private FlashSet getFormData() {
+        return new FlashSet(new ArrayList<FlashCard>(),
+                ((EditText) findViewById(R.id.editText_set_title)).getText().toString(),
+                ((EditText) findViewById(R.id.editText_set_author)).getText().toString(),
+                "",
+                new int[0]
+        );
     }
 }
---
+++ app/src/main/java/com/example/strik/lafa/ModifyFlashSetActivity.java
@@ -0,0 +1,13 @@
+package com.example.strik.lafa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModifyFlashSetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_flash_set);
+    }
+}
